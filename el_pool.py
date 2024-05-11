0c0f51c35a774943deceabe52d42b4fe01003786
Анастасия Штомпель <anastasiastompel@MacBook-Pro-Anastasia.local>
2024-05-12 01:25:01 +0300

    rename script to el pool

--- el_pool.py
+++ el_pool.py
@@ -4,16 +4,18 @@ import warnings
 import cooler
 import cooltools
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import scipy
 import statsmodels.stats.multitest
 from coolpuppy import coolpup, plotpup
-from sklearn.cluster import DBSCAN, OPTICS
+from sklearn.cluster import OPTICS
 
 warnings.simplefilter(action='ignore')
+np.seterr(divide='ignore')
 
 def get_qvalues(path_to_matrix, resolution, genome_position, end_bin, start_bin, quantile_threshold, fdr_correction):
+    """
+    """
     mtx_name_for_cooler = path_to_matrix + '::/resolutions/' + str(resolution)
     balanced_matrix = cooler.Cooler(mtx_name_for_cooler).matrix(balance=True, sparse=False).fetch(genome_position)
     compare_dataframe = pd.DataFrame()
@@ -31,6 +33,8 @@ def get_qvalues(path_to_matrix, resolution, genome_position, end_bin, start_bin,
 
 
 def create_qval_log_mtx(qval_df, end_bin):
+    """
+    """
     temp_mtx = np.zeros((qval_df.shape[0] + 1,qval_df.shape[0] + 1))
     for i in range(1, end_bin):
         upper = qval_df[i - 1]
@@ -40,6 +44,8 @@ def create_qval_log_mtx(qval_df, end_bin):
 
 
 def get_dots_coords(qval_log_mtx, qval_threshold):
+    """
+    """
     values = np.where((~np.isneginf(qval_log_mtx)) & (qval_log_mtx != 0) & (qval_log_mtx<=np.log10(qval_threshold)))
     coords = pd.DataFrame()
     for i, j in zip(values[0], values[1]):
@@ -50,28 +56,9 @@ def get_dots_coords(qval_log_mtx, qval_threshold):
     return coords
 
 
-def dbscan_clustering(coordinates, eps, min_samples, genome_position, resolution):
-    coor = coordinates[['start', 'end']]
-    clusterer = DBSCAN(eps=eps, min_samples = min_samples)
-    cluster_labels_all_db = clusterer.fit_predict(coor)
-    coor['dbscan'] = cluster_labels_all_db
-    coor_dbscan = coor.query('dbscan != -1')
-    loops_coords_bedpe = pd.DataFrame()
-    labels = coor_dbscan['dbscan'].unique()
-    for i in labels:
-        new_row = round(coor_dbscan.query('dbscan == @i')[{'start', 'end'}].apply(np.mean, axis=0)).astype(int)
-        loops_coords_bedpe = pd.concat([loops_coords_bedpe, pd.DataFrame([new_row])], ignore_index=True)
-    chrom_name = genome_position if ':' not in genome_position else genome_position.split(':')[0]
-    loops_coords_bedpe['chrom1'] = chrom_name
-    loops_coords_bedpe[['start1', 'start2']] = loops_coords_bedpe[['start', 'end']].apply(lambda x: (x - 1) * resolution)
-    loops_coords_bedpe['chrom2'] = chrom_name
-    loops_coords_bedpe[['end1', 'end2']] = loops_coords_bedpe[['start', 'end']].apply(lambda x: (x + 1) * resolution)
-    loops_coords_bedpe = loops_coords_bedpe.drop(['start', 'end'], axis=1)
-    loops_coords_bedpe = loops_coords_bedpe.query('start1 >= 300000')
-    return loops_coords_bedpe
-
-
 def optics_clusterig(coordinates, min_samples, max_eps, genome_position, resolution):
+    """
+    """
     coor = coordinates[['start', 'end']]
     optics = OPTICS(min_samples = min_samples, max_eps = max_eps)
     optics_pred = optics.fit_predict(coor)
@@ -84,15 +71,17 @@ def optics_clusterig(coordinates, min_samples, max_eps, genome_position, resolut
         loops_coords_bedpe = pd.concat([loops_coords_bedpe, pd.DataFrame([new_row])], ignore_index=True)
     chrom_name = genome_position if ':' not in genome_position else genome_position.split(':')[0]
     loops_coords_bedpe['chrom1'] = chrom_name
-    loops_coords_bedpe[['start1', 'start2']] = loops_coords_bedpe[['start', 'end']].apply(lambda x: (x - 1) * resolution)
+    loops_coords_bedpe[['start1', 'start2']] = loops_coords_bedpe[['start', 'end']].apply(lambda x: (x - 5) * resolution)
     loops_coords_bedpe['chrom2'] = chrom_name
-    loops_coords_bedpe[['end1', 'end2']] = loops_coords_bedpe[['start', 'end']].apply(lambda x: (x + 1) * resolution)
+    loops_coords_bedpe[['end1', 'end2']] = loops_coords_bedpe[['start', 'end']].apply(lambda x: (x + 5) * resolution)
     loops_coords_bedpe = loops_coords_bedpe.drop(['start', 'end'], axis=1)
     loops_coords_bedpe = loops_coords_bedpe.query('start1 >= 300000')
     return loops_coords_bedpe
     
 
-def pileup_dots(loop_coords, path_to_matrix, resolution):
+def pileup_dots(loop_coords, path_to_matrix, resolution, visualization=False):
+    """
+    """
     mtx_name_for_cooler = path_to_matrix + '::/resolutions/' + str(resolution)
     hic = cooler.Cooler(mtx_name_for_cooler)
     sizes = hic.chromsizes
@@ -105,19 +94,86 @@ def pileup_dots(loop_coords, path_to_matrix, resolution):
     view['name'] = sizes['name']
     expected = cooltools.expected_cis(hic, chunksize=1000000, view_df=view)
     puppy = coolpup.pileup(clr=hic, features=loop_coords, view_df=view, features_format='bedpe', expected_df=expected, 
-                            nshifts=1000, flank=12000)
-    pile_loops = plotpup.plot(puppy, score=True, cmap='coolwarm', scale='log', sym=True, vmax=2,height=5, plot_ticks=False)
-    return pile_loops
+                            nshifts=100000, flank=8500)
+    if visualization:
+        pile_loops = plotpup.plot(puppy, score=True, cmap='coolwarm', scale='log', sym=True, vmax=2.5, height=5, plot_ticks=False)
+    return puppy
 
 
-def main_func(path_to_matrix, resolution, genome_position, end_bin, cluster_method = 'dbscan', start_bin=1, quantile_threshold = 0.9, fdr_correction = 0.5, qval_threshold = 0.01, eps=1, min_samples = 3, max_eps=1.5):
+def create_mask(puppy, elong):
+    """
+    """
+    puppy = puppy.data[0]
+    peak = puppy.max()
+    lowest_value = peak - puppy.min()
+    itemindex = np.where(puppy == peak)
+    row = itemindex[0][0]
+    column = itemindex[1][0]
+    row_iter = row
+    column_iter = column
+    while puppy[row_iter][column] > lowest_value and (row_iter < puppy.shape[0]-1):
+        row_iter = row_iter+1
+    value = puppy[row_iter-1][column]
+    item_row = np.where(puppy == value)
+    while (puppy[row][column_iter] > lowest_value) and (column_iter < puppy.shape[0]-1):
+        column_iter = column_iter+1
+    value = puppy[row][column_iter-2]
+    item_column = np.where(puppy == value)
+    end1 = item_row[0][0]
+    end2 = item_column[1][0]
+    vl = end1 - row
+    vl_2 = end2 - column
+    pp = puppy[row - vl:row+vl, column - vl_2 :column+vl_2]
+    pp = pp * -1
+    if pp.shape != (2,2):
+        pp = np.random.choice(pp.flatten(), (2,2))
+    if elong == 'left':
+        pp = np.repeat(pp, 2, axis=1)
+        pp = np.pad(pp, [(4, ), (3, )], mode='constant')
+    elif elong == 'right':
+        pp = np.repeat(pp,2, axis=0)
+        pp = np.pad(pp, [(3, ), (4, )], mode='constant')
+    elif elong is None:
+        pp = np.repeat(np.repeat(pp,2, axis=0), 2, axis=1)
+        pp = np.pad(pp, [(3, ), (3, )], mode='constant')
+    return pp
+
+def count_mask_vs_loop_corelation(pp, coords, log_mtx):
+    """
+    """
+    dict_for_coeffs = {}
+    threshold = 0.5
+    for i in range(len(coords)):
+        coordinates_dots = coords.iloc[i][['start1', 'start2', 'end1', 'end2']].apply(lambda x: x //2000)
+        start_1, start_2, end_1, end_2 = [j for j in coordinates_dots]
+        mtx = log_mtx[start_1:end_1, start_2:end_2]
+        if mtx.shape != pp.shape:
+            continue
+        mtx = np.nan_to_num(mtx, neginf=0) 
+        v1 = (mtx).flatten()
+        v2 = (pp).flatten()
+        coeff = scipy.stats.spearmanr(v1, v2)[0]
+        if coeff >= threshold:
+            dict_for_coeffs[i] = scipy.stats.spearmanr(v1, v2)[0]
+    coords_elog = pd.DataFrame()
+    for i in dict_for_coeffs:
+        row = list(coords.iloc[i])
+        row = pd.DataFrame([row])
+        coords_elog = pd.concat([coords_elog, row])
+    col_names = ['chrom1', 'start1', 'start2', 'chrom2', 'end1', 'end2']
+    coords_elog = coords_elog.set_axis(col_names, axis=1)
+    return coords_elog
+
+
+def run_elong_loop_caller(path_to_matrix, resolution, genome_position, end_bin, start_bin=1, quantile_threshold = 0.9, fdr_correction = 0.5, qval_threshold = 0.01, min_samples = 3, max_eps=1.5, elong='left'):
+    """
+    """
     qval_dataframe = get_qvalues(path_to_matrix, resolution, genome_position, end_bin, start_bin, quantile_threshold, fdr_correction)
-    np.seterr(divide='ignore')
     log_matrix = create_qval_log_mtx(qval_dataframe, end_bin)
     sign_dots = get_dots_coords(log_matrix, qval_threshold)
-    if cluster_method == 'dbscan':
-        loops_genome_coords = dbscan_clustering(sign_dots, eps, min_samples, genome_position, resolution)
-    elif cluster_method == 'optics':
-        loops_genome_coords = optics_clusterig(sign_dots, min_samples, max_eps, genome_position, resolution)
-    draw_average_loops = pileup_dots(loops_genome_coords, path_to_matrix, resolution)
-    return draw_average_loops
+    loops_genome_coords = optics_clusterig(sign_dots, min_samples, max_eps, genome_position, resolution)
+    puppy_data = pileup_dots(loops_genome_coords, path_to_matrix, resolution)
+    mask = create_mask(puppy_data, elong)
+    elong_loop_df = count_mask_vs_loop_corelation(mask, loops_genome_coords, log_matrix)
+    elong_pileup = pileup_dots(elong_loop_df, path_to_matrix, resolution, visualization=True)
+    return elong_loop_df
